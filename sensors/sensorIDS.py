cffaac56e9bde4af773f5350ddce1f17dd21876b
CosminRadoi <cosmin.radoi.001@student.uni.lui>
2023-05-15 14:15:35 +0200

    [Fix] - changed the implementation into a class

--- sensors/sensorIDS.py
+++ sensors/sensorIDS.py
@@ -3,248 +3,192 @@ from ids_peak_ipl import ids_peak_ipl
 import numpy as np
 
 
-def createCameraObjectIDS(port):
-    '''
-    Creates an IDS Device object and a nodemap.
+class idsCamera:
+    def __init__(self, port):
+        '''
+        IDS Camera class for interacting with IDS peak library.
 
-    Parameters
-    ----------
-    port: int
-        port number of the camera that is connected
+        Parameters
+        ----------
+        port : int
+            Port number of the camera that is connected.
+        '''
+        # initialize the camera manager
+        ids_peak.Library.Initialize()
+        device_manager = ids_peak.DeviceManager.Instance()
+        device_manager.Update()
 
-    Returns
-    -------
-    cap: ids_peak.Device (unsure of the type)
-        camera capture object
-    nodemap: ids_peak.Nodemap
-        camera nodemap
-    '''
-    # initialize the camera manager
-    ids_peak.Library.Initialize()
-    device_manager = ids_peak.DeviceManager.Instance()
-    device_manager.Update()
+        self.cap = device_manager.Devices()[port].OpenDevice(
+            ids_peak.DeviceAccessType_Exclusive)
 
-    cap = device_manager.Devices()[port].OpenDevice(
-        ids_peak.DeviceAccessType_Exclusive)
+        self.nodemap = self.cap.RemoteDevice().NodeMaps()[0]
 
-    nodemap = cap.RemoteDevice().NodeMaps()[0]
+        self.nodemap.FindNode(
+            "AcquisitionMode").SetCurrentEntry("Continuous")
+        self.datastream = None
+        self.buffer = None
+        self.frame = None
 
-    nodemap.FindNode(
-        "AcquisitionMode").SetCurrentEntry("Continuous")
+    def loadCameraParameters(self, file):
+        '''
+        Loads camera parameters from a yaml file.
 
-    return cap, nodemap
+        Parameters
+        ----------
+        file : str
+            Path to the yaml file containing camera parameters.
 
+        Raises
+        ------
+        Exception
+            If an error occurs while loading parameters from the file.
+        '''
+        try:
+            self.nodemap.LoadFromFile(file)
+        except Exception as exception:
+            print(
+                f'Error occurred in loadCameraParameters!\n{exception}', 'error')
 
-def loadCameraParameters(nodemap, file):
-    '''
-    Loads camera parameters from a yaml file.
+    def syncAsMaster(self):
+        '''
+        Synchronizes the camera as a master.
 
-    Parameters
-    ----------
-    nodemap: ids_peak.Nodemap
-        camera nodemap
-    file: str
-        path to the yaml file containing camera parameters
+        Raises
+        ------
+        Exception
+            If an error occurs while synchronizing the camera.
+        '''
+        try:
+            self.nodemap.FindNode(
+                "TimerSelector").SetCurrentEntry("Timer0")
+            self.nodemap.FindNode("TimerDuration").SetValue(1000)
+            self.nodemap.FindNode(
+                "TimerTriggerSource").SetCurrentEntry("Off")
 
-    Raises
-    ------
-    Exception:
-        if an error occurs while loading parameters from file
-    '''
-    try:
-        nodemap.LoadFromFile(file)
-    except Exception as exception:
-        print(
-            f'Error occurred in loadCameraParameters!\n{exception}', 'error')
+            self.nodemap.FindNode("LineSelector").SetCurrentEntry("Line2")
+            self.nodemap.FindNode("LineMode").SetCurrentEntry("Output")
+            self.nodemap.FindNode(
+                "LineSource").SetCurrentEntry("Timer0Active")
 
+            self.nodemap.FindNode("LineInverter").SetValue(True)
 
-def syncAsMaster(nodemap):
-    '''
-    Synchronizes the camera as a master.
+            self.nodemap.FindNode(
+                "TriggerSelector").SetCurrentEntry("ExposureStart")
+            self.nodemap.FindNode("TriggerMode").SetCurrentEntry("On")
+            self.nodemap.FindNode(
+                "TriggerSource").SetCurrentEntry("Line2")
+            self.nodemap.FindNode(
+                "TriggerActivation").SetCurrentEntry("RisingEdge")
+        except Exception as exception:
+            print(f'Error occurred in syncAsMaster!\n{exception}', 'error')
 
-    Parameters
-    ----------
-    nodemap: ids_peak.Nodemap
-        camera nodemap
+    def syncAsSlave(self):
+        '''
+        Synchronizes the camera as a slave.
 
-    Returns
-    -------
-    bool:
-        True if synchronization is successful, False otherwise
+        Raises
+        ------
+        Exception
+            If an error occurs while synchronizing the camera.
+        '''
+        try:
+            self.nodemap.FindNode(
+                "LineSelector").SetCurrentEntry("Line2")
+            self.nodemap.FindNode("LineMode").SetCurrentEntry("Input")
 
-    Raises
-    ------
-    Exception:
-        if an error occurs while synchronizing the camera
-    '''
-    try:
-        nodemap.FindNode(
-            "TimerSelector").SetCurrentEntry("Timer0")
-        nodemap.FindNode("TimerDuration").SetValue(1000)
-        nodemap.FindNode(
-            "TimerTriggerSource").SetCurrentEntry("Off")
+            self.nodemap.FindNode(
+                "TriggerSelector").SetCurrentEntry("ExposureStart")
 
-        nodemap.FindNode("LineSelector").SetCurrentEntry("Line2")
-        nodemap.FindNode("LineMode").SetCurrentEntry("Output")
-        nodemap.FindNode(
-            "LineSource").SetCurrentEntry("Timer0Active")
+            self.nodemap.FindNode("TriggerMode").SetCurrentEntry("On")
+            self.nodemap.FindNode(
+                "TriggerSource").SetCurrentEntry("Line2")
+            self.nodemap.FindNode(
+                "TriggerActivation").SetCurrentEntry("RisingEdge")
+        except Exception as exception:
+            print(
+                f'Error occurred in syncAsSlave!\n{exception}', 'error')
 
-        nodemap.FindNode("LineInverter").SetValue(True)
+    def startAquisition(self):
+        '''
+        Starts the data acquisition from the camera.
 
-        nodemap.FindNode(
-            "TriggerSelector").SetCurrentEntry("ExposureStart")
-        nodemap.FindNode("TriggerMode").SetCurrentEntry("On")
-        nodemap.FindNode(
-            "TriggerSource").SetCurrentEntry("Line2")
-        nodemap.FindNode(
-            "TriggerActivation").SetCurrentEntry("RisingEdge")
-        return True
-    except Exception as exception:
-        print(
-            f'Error occurred in syncAsMaster!\n{exception}', 'error')
-        return False
+        Raises
+        ------
+        Exception
+            If an error occurs while starting the acquisition.
+        '''
+        try:
+            self.datastream = self.cap.DataStreams()[0].OpenDataStream()
+            self.payload_size = self.nodemap.FindNode("PayloadSize").Value()
+            for i in range(self.datastream.NumBuffersAnnouncedMinRequired()):
+                buffer = self.datastream.AllocAndAnnounceBuffer(
+                    self.payload_size)
+                self.datastream.QueueBuffer(buffer)
 
+            self.datastream.StartAcquisition()
+            self.nodemap.FindNode("AcquisitionStart").Execute()
+            self.nodemap.FindNode("AcquisitionStart").WaitUntilDone()
+        except Exception as exception:
+            print(
+                f'Error occurred in startAquisition!\n{exception}', 'error')
 
-def syncAsSlave(nodemap):
-    '''
-    Synchronizes the camera as a slave.
+    def setExposureTime(self, exposureTime):
+        '''
+        Sets the exposure time for the camera.
 
-    Parameters
-    ----------
-    nodemap: ids_peak.Nodemap
-        camera nodemap
+        Parameters
+        ----------
+        exposureTime : float
+            The exposure time to set in milliseconds.
 
-    Returns
-    -------
-    bool:
-        True if synchronization is successful, False otherwise
+        Raises
+        ------
+        Exception
+            If an error occurs while setting the exposure time.
+        '''
+        try:
+            self.nodemap.FindNode("ExposureTime").SetValue(exposureTime)
+        except Exception as exception:
+            print(
+                f'Error occurred in setExposureTime!\n{exception}', 'error')
 
-    Raises
-    ------
-    Exception:
-        if an error occurs while synchronizing the camera
-    '''
-    try:
-        nodemap.FindNode(
-            "LineSelector").SetCurrentEntry("Line2")
-        nodemap.FindNode("LineMode").SetCurrentEntry("Input")
+    def getFrame(self):
+        '''
+        Triggers the camera to capture a frame and returns the frame as a numpy array.
 
-        nodemap.FindNode(
-            "TriggerSelector").SetCurrentEntry("ExposureStart")
+        Returns
+        -------
+        np.ndarray
+            The captured frame as a numpy array.
 
-        nodemap.FindNode("TriggerMode").SetCurrentEntry("On")
-        nodemap.FindNode(
-            "TriggerSource").SetCurrentEntry("Line2")
-        nodemap.FindNode(
-            "TriggerActivation").SetCurrentEntry("RisingEdge")
-        return True
-    except Exception as exception:
-        print(
-            f'Error occurred in syncAsSlave!\n{exception}', 'error')
-        return False
+        Raises
+        ------
+        Exception
+            If an error occurs while capturing the frame.
+        '''
+        try:
+            # trigger image
+            self.nodemap.FindNode("TimerReset").Execute()
 
+            self.buffer = self.datastream.WaitForFinishedBuffer(1000)
 
-def startAquisition(cap, nodemap):
-    '''
-    Starts the data acquisition from the camera and returns the data stream and buffer.
+            # obtain image and convert from IPL to numpy array
+            self.raw_image = ids_peak_ipl.Image_CreateFromSizeAndBuffer(self.buffer.PixelFormat(
+            ), self.buffer.BasePtr(), self.buffer.Size(), self.buffer.Width(), self.buffer.Height())
 
-    Parameters
-    ----------
-    cap: ids_peak.Device
-        camera capture object
-    nodemap: ids_peak.Nodemap
-        camera nodemap
+            self.color_image = self.raw_image.ConvertTo(
+                ids_peak_ipl.PixelFormatName_BGR8)
 
-    Returns
-    -------
-    datastream: ids_peak.DataStream
-        the data stream for the camera
-    buffer: ids_peak.Buffer
-        the buffer for the data stream
-    '''
-    try:
-        datastream = cap.DataStreams()[0].OpenDataStream()
-        payload_size = nodemap.FindNode("PayloadSize").Value()
-        for i in range(datastream.NumBuffersAnnouncedMinRequired()):
-            buffer = datastream.AllocAndAnnounceBuffer(payload_size)
-            datastream.QueueBuffer(buffer)
+            # queue buffer to be used again
+            self.datastream.QueueBuffer(self.buffer)
 
-        datastream.StartAcquisition()
-        nodemap.FindNode("AcquisitionStart").Execute()
-        nodemap.FindNode("AcquisitionStart").WaitUntilDone()
-        return datastream, buffer
-    except Exception as exception:
-        print(
-            f'Error occurred in startAquisition!\n{exception}', 'error')
+            self.frame = self.color_image.get_numpy_3D()
 
+            return self.frame
 
-def setExposureTime(nodemap, exposureTime):
-    '''
-    Sets the exposure time for the camera.
+        except Exception as exception:
+            print(
+                f'Error occurred in getFrame!\n{exception}', 'error')
 
-    Parameters
-    ----------
-    nodemap: ids_peak.Nodemap
-        camera nodemap
-    exposureTime: float
-        the exposure time to set in ms
-
-    Returns
-    -------
-    bool
-        True if the exposure time was successfully set, False otherwise
-    '''
-    try:
-        nodemap.FindNode("ExposureTime").SetValue(exposureTime)
-        return True
-    except Exception as exception:
-        print(
-            f'Error occurred in setExposureTime!\n{exception}', 'error')
-        return False
-
-
-def getFrame(nodemap, datasream, buffer):
-    '''
-    Triggers the camera to capture a frame and returns the frame as a numpy array.
-
-    Parameters
-    ----------
-    nodemap: ids_peak.Nodemap
-        camera nodemap
-    datastream: ids_peak.DataStream
-        the data stream for the camera
-    buffer: ids_peak.Buffer
-        the buffer for the data stream
-
-    Returns
-    -------
-    np.ndarray
-        the captured frame as a numpy array
-    '''
-    try:
-        # trigger image
-        nodemap.FindNode("TimerReset").Execute()
-
-        buffer = datasream.WaitForFinishedBuffer(1000)
-
-        # obtain image and convert from IPL to numpy array
-        raw_image = ids_peak_ipl.Image_CreateFromSizeAndBuffer(buffer.PixelFormat(
-        ), buffer.BasePtr(), buffer.Size(), buffer.Width(), buffer.Height())
-
-        color_image = raw_image.ConvertTo(
-            ids_peak_ipl.PixelFormatName_BGR8)
-
-        # queue buffer to be used again
-        datasream.QueueBuffer(buffer)
-
-        frame = color_image.get_numpy_3D()
-
-        return frame
-
-    except Exception as exception:
-        print(
-            f'Error occurred in getFrame!\n{exception}', 'error')
-
-
-def closeLibrary():
-    ids_peak.Library.Close()
+    def closeLibrary(self):
+        ids_peak.Library.Close()
