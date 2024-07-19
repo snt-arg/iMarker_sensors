aec3bd5ee11956b2c7bd7c7834972f4275331764
Ali Tourani <a.tourani1991@gmail.com>
2024-07-19 11:31:33 +0200

    [feat] - improve sensors

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(name='csr_sensors',
-      version='1.0',
+      version='1.2',
       author='Ali Tourani',
       description='CSR detector sensors',
       url='https://github.com/snt-arg/csr_sensors',
@@ -10,6 +10,8 @@ setup(name='csr_sensors',
       install_requires=[
           'numpy',
           'opencv-python',
-          'pyrealsense2'
+          'pyrealsense2',
+          'ids-peak',
+          'ids-peak-ipl'
       ],
       )
