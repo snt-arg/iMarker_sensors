3382015dee1700a049c7f72aabe3d58077ddb36e
Ali Tourani <a.tourani1991@gmail.com>
2024-08-02 17:59:33 +0200

    [fix] - resolve issue with setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,18 @@
 from setuptools import find_packages, setup
 
-setup(name='csr_sensors',
-      version='1.2',
-      author='Ali Tourani',
-      description='CSR detector sensors',
-      url='https://github.com/snt-arg/csr_sensors',
-      packages=find_packages(
-          include=['sensors']),
-      install_requires=[
-          'numpy',
-          'opencv-python',
-          'pyrealsense2',
-          'ids-peak',
-          'ids-peak-ipl'
-      ],
-      )
+if __name__ == '__main__':
+    setup(name='csr_sensors',
+          version='1.2',
+          author='Ali Tourani',
+          description='CSR detector sensors',
+          url='https://github.com/snt-arg/csr_sensors',
+          packages=find_packages(
+              include=['sensors']),
+          install_requires=[
+              'numpy',
+              'opencv-python',
+              'pyrealsense2',
+              'ids-peak',
+              'ids-peak-ipl'
+          ],
+          )
