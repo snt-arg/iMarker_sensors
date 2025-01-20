0b252056f26d5f237e0e5a35c9d8880072b92420
Ali Tourani <a.tourani1991@gmail.com>
2025-01-20 18:56:07 +0100

    [feat] - add RS presets

---
+++ sensors/config/rsPresets.py
@@ -0,0 +1,9 @@
+# RealSense Configurations
+import numpy as np
+
+cameraMatrix = np.array(
+    [[605.8, 0.0, 325.0],
+        [0.0, 606.3, 244.8],
+        [0.0, 0.0, 1.0]])
+
+distCoeffs = np.array([0.0, 0.0, 0.0, 0.0, 0.0])
